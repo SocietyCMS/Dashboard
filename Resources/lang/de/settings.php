40b177221928f7935fb78b7b1445431c36d34420
Ralph Huwiler <ralph@huwiler.rocks>
2016-05-07 18:01:25 +0200

    german localization

---
+++ Resources/lang/de/settings.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'landingpage' => [
+        'title' => 'Startseite',
+
+        'landingpage-type' => [
+            'title' => 'Gib den Typ der Startseite an',
+
+            'use-dynamic-landingpage' => 'dynamische Startseite',
+            'use-static-landingpage'  => 'statische Startseite',
+        ],
+    ],
+
+    'dashboard' => [
+        'title' => 'Dashboard',
+
+        'welcome-message'  => [
+            'title'       => 'Willkommensnachricht',
+            'description' => 'Du kannst :user oder :firstname verwenden, um den aktuellen Benutzer anzusprechen.',
+        ],
+        'welcome-subtitle' => [
+            'title'       => 'Willkommensnachricht Untertitel',
+        ],
+    ],
+
+];
